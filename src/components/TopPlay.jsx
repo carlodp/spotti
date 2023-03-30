0bb12c72c90f2148b21c0ffaf764b746e4242750
carlodp <santos.carlodp@gmail.com>
2023-03-30 18:23:06 +0800

    Update TopPlay.jsx

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -22,13 +22,13 @@ const TopChartCard = ({
 }) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
     <h3 className="font-bold text-base text-white mr-3">{i + 1}</h3>
-    <div className="flex-1 flex flex-row items-center">
+    <div className="flex-1 flex flex-row justify-between items-center">
       <img
         src={song?.images?.coverart}
         alt={song?.title}
         className="w-20 h-20 rounded-lg"
       />
-      <div className="flex 1 flex flex-col justify-center mx-3">
+      <div className="flex-1 flex flex-col justify-center mx-3">
         <Link to={`/songs/${song.key}`}>
           <p className="text-xl font-bold text-white">{song?.title}</p>
         </Link>
@@ -58,8 +58,6 @@ const TopPlay = () => {
   });
 
   const topPlays = data?.tracks.slice(0, 5);
-
-  console.log(topPlays);
 
   const handlePauseClick = () => {
     dispatch(playPause(false));
