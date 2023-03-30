a8caec89676d16317a9857a1bb322e38443ca14e
carlodp <santos.carlodp@gmail.com>
2023-03-30 18:01:41 +0800

    TopPlay update

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -12,9 +12,38 @@ import { useGetChartsQuery } from "../redux/services/shazamCore";
 import "swiper/css";
 import "swiper/css/free-mode";
 
-const TopChartCard = ({ song, i }) => (
+const TopChartCard = ({
+  song,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+  i,
+}) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-    {song.title}
+    <h3 className="font-bold text-base text-white mr-3">{i + 1}</h3>
+    <div className="flex-1 flex flex-row items-center">
+      <img
+        src={song?.images?.coverart}
+        alt={song?.title}
+        className="w-20 h-20 rounded-lg"
+      />
+      <div className="flex 1 flex flex-col justify-center mx-3">
+        <Link to={`/songs/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+        <Link to={`/songs/${song.artists[0].adamid}`}>
+          <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
+        </Link>
+      </div>
+    </div>
+    <PlayPause
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePauseClick={handlePauseClick}
+      handlePlay={handlePlayClick}
+    />
   </div>
 );
 
@@ -56,7 +85,15 @@ const TopPlay = () => {
 
         <div className="mt-4 flex flex-col gap-1">
           {topPlays?.map((song, i) => (
-            <TopChartCard key={song.key} song={song} i={i} />
+            <TopChartCard
+              key={song.key}
+              song={song}
+              i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePause={handlePauseClick}
+              handlePlay={handlePlayClick}
+            />
           ))}
         </div>
       </div>
