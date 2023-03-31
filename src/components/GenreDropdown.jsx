3118aab95363809852c0319c1c2ad778fd6304d2
carlodp <santos.carlodp@gmail.com>
2023-03-31 12:18:31 +0800

    genre dropdown

---
+++ src/components/GenreDropdown.jsx
@@ -0,0 +1,36 @@
+import { genres } from "../assets/constants";
+
+const GenreDropdown = ({ genreName, genreListId, genreHandler }) => {
+  const genreChangeHandler = (event) => {
+    genreHandler(event);
+  };
+
+  return (
+    <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+      <h2 className="font-bold text-3xl text-white text-left">
+        Top Songs on {genreName}
+      </h2>
+      
+      <select
+        onChange={genreChangeHandler}
+        value={genreListId}
+        className="genre-select bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5 cursor-pointer lg:w-[200px]"
+        name="genre"
+      >
+        {genres.map((genre) => (
+          <option
+            key={genre.value}
+            value={genre.value}
+            name={genre.title}
+            onClick={genreChangeHandler}
+            className="p-2"
+          >
+            {genre.title}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default GenreDropdown;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -4,11 +4,11 @@ import { genres } from "../assets/constants";
 import { useGetChartsQuery } from "../redux/services/shazamCore";
 import { useState } from "react";
 import { selectGenreListId } from "../redux/features/playerSlice";
+import GenreDropdown from "../components/GenreDropdown";
 
 
 const Discover = () => {
   const dispatch = useDispatch();
-  const [genre, setGenre] = useState('');
   const [genreName, setGenreName] = useState('Global');
   const { activeSong, isPlaying, genreListId } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetChartsQuery(genreListId);
@@ -19,30 +19,12 @@ const Discover = () => {
   const genreHandler = (event) => {
     const index = event.nativeEvent.target.selectedIndex;
     setGenreName(event.nativeEvent.target[index].text);
-
     dispatch(selectGenreListId(event.target.value));
   }
 
   return (
     <div className="flex flex-col">
-      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-        <h2 className="font-bold text-3xl text-white text-left">
-          {genreName}
-        </h2>
-        <select
-          onChange={genreHandler}
-          value={genreListId}
-          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
-          name="genre"
-        >
-          {genres.map((genre) => (
-            <option key={genre.value} value={genre.value} name={genre.title} onClick={genreHandler}>
-              {genre.title}
-            </option>
-          ))}
-        </select>
-      </div>
-
+      <GenreDropdown genreName={genreName} genreListId={genreListId} genreHandler={genreHandler} />
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data.tracks?.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} data={data} isPlaying={isPlaying} activeSong={activeSong} />
